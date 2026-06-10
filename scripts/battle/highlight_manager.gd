a8061f0b265349a4e74f886e98db6de6b1331fcd
rcarbonaro <rcarbonaro119@gmail.com>
2026-06-10 17:25:13 -0400

    added magic

--- scripts/battle/highlight_manager.gd
+++ scripts/battle/highlight_manager.gd
@@ -61,3 +61,19 @@ func clear_highlights() -> void:
 		node.queue_free()
 
 	_highlight_nodes.clear()
+	
+
+
+func highlight_aoe_blast_cells(cells: Array) -> void:
+	# Note: We do NOT clear highlights here because we want to layer 
+	# this orange/yellow layout directly on top of the red range tiles!
+	for cell in cells:
+		var rect = ColorRect.new()
+		rect.size = Vector2(TILE_SIZE, TILE_SIZE)
+		rect.position = grid.grid_to_world(cell) - Vector2(TILE_SIZE / 2.0, TILE_SIZE / 2.0)
+		
+		# Bright orange warning overlay (Red: 1.0, Green: 0.5, Blue: 0.0, Opacity: 0.4)
+		rect.color = Color(1.0, 0.5, 0.0, 0.4)  
+		rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		add_child(rect)
+		_highlight_nodes.append(rect)
